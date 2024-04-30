6d622afd9d59361bef6225eeda105082f2c5f0d7
Liyons Varghese <liyonsvarghese@gmail.com>
2024-04-30 11:44:46 +0530

    Collectable Items
    Coin Rotate Function
    Enemy car move function
    Prefabs Updation
    Level tile creation

--- Assets/Scripts/Targets/CoinManager.cs
+++ Assets/Scripts/Targets/CoinManager.cs
@@ -4,6 +4,16 @@ namespace EndlessRunner.Targets
 {
     class CoinManager : BaseTarget
     {
+        [SerializeField] private float coinRotateSpeed;
+        private void Update()
+        {
+            RotateCoin();
+        }
+
+        private void RotateCoin()
+        {
+            transform.Rotate(0, 1 * coinRotateSpeed * Time.deltaTime, 0);
+        }
         public override void OnPlayerHit()
         {
             Debug.Log("Coin Hit");
--- Assets/Scripts/Targets/EnemyCarManager.cs
+++ Assets/Scripts/Targets/EnemyCarManager.cs
@@ -4,6 +4,17 @@ namespace EndlessRunner.Targets
 {
     class EnemyCarManager : BaseTarget
     {
+        [SerializeField] private float enemyCarSpeed;
+
+        private void Update()
+        {
+            MoveForward();
+        }
+
+        private void MoveForward()
+        {
+            transform.position += enemyCarSpeed * Time.deltaTime * transform.forward;
+        }
         public override void OnPlayerHit()
         {
             Debug.Log("Enemy car hit");
