4463ea3f9d59cf3331bc90474a4c5e37a8a379cb
Liyons Varghese <liyonsvarghese@gmail.com>
2024-04-30 14:17:03 +0530

    Spawning player dynamically

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -5,8 +5,19 @@ namespace EndlessRunner
 {
     public class PlayerService : BaseMonoSingletonGeneric<PlayerService>
     {
-        [SerializeField] private Transform playerTransform;
-
+        [SerializeField] private GameObject playerPrefab;
+        private Transform playerTransform;
         public Transform PlayerPos => playerTransform;
+
+        private void Start()
+        {
+            SpawnPlayer();
+        }
+
+        private void SpawnPlayer()
+        {
+            playerTransform = Instantiate(playerPrefab, this.transform).transform;
+        }
+       
     }
 }
