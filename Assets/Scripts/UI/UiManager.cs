77039090157c448406c1463438b9bc63da29c5ed
Liyons Varghese <liyonsvarghese@gmail.com>
2024-04-30 15:07:02 +0530

    Updated

--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
+using TMPro;
 
 namespace EndlessRunner.UI
 {
     public class UiManager : MonoBehaviour
     {
-       
+        [SerializeField] private GameObject gameUi;
+        [SerializeField] private GameObject gameOverPanel;
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private TextMeshProUGUI coinsText;
+
+        private void Start()
+        {
+            OnGameStart();
+        }
+     
+        private void OnGameStart()
+        {
+            gameUi.SetActive(true);
+            SetScoreText(0);
+            SetCoinsText(0);
+        }
+
+        private void OnGameOver()
+        {
+            gameUi.SetActive(false);
+            gameOverPanel.SetActive(true);
+        }
+
+        private void SetScoreText(int value)
+        {
+            scoreText.text = "Score : " + value.ToString();
+        }
+
+        private void SetCoinsText(int value)
+        {
+            coinsText.text = "Coins : " + value.ToString();
+        }
     }
 }
