d8c6816471d507bd206ed7af9effd1d84985098f
Liyons Varghese <liyonsvarghese@gmail.com>
2024-05-02 17:00:44 +0530

    Bug fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -45,7 +45,7 @@ namespace EndlessRunner.Player
                 if (Input.touchCount > 0)
                 {
                     Touch touch = Input.GetTouch(0);
-                    if (touch.phase == TouchPhase.Began)
+                    if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
                     {
                         if (touch.position.x < Screen.width / 2)
                         {
